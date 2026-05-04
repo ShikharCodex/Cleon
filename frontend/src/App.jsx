d9ae92df57c84b5703caf042be9252f13c115abc
Shikhar <shikharcodex@gmail.com>
2026-05-04 17:40:21 +0530

    feat: integrate Sentry for error tracking and user context synchronization
    - Added Sentry for error tracking and user context synchronization with Clerk.
    - Implemented SentryErrorFallback component for error boundaries.
    - Created SentryUserSync component to keep Sentry user context in sync with Clerk.
    - Updated main entry point to include Sentry initialization and error boundary.
    - Added PageLoader component for loading state management.
    - Introduced Layout component to wrap application structure with Navbar and Footer.
    - Created Footer and Navbar components for improved UI structure.
    - Integrated React Query for data fetching and state management.
    - Updated package.json to include new dependencies for Sentry, Tailwind CSS, and React Query.
    - Refactored index.css to use Tailwind CSS and DaisyUI for styling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { SignInButton, SignUpButton, UserButton, Show } from "@clerk/react";
 import "./App.css";
+import PageLoader from "./components/PageLoader";
+import { useAuth } from "@clerk/react";
+import Layout from "./components/Layout";
 
 function App() {
+
+  const {isLoaded} = useAuth();
+  if(!isLoaded) return <PageLoader />;
   return (
-    <>
+    <Layout>
       <header>
         <Show when="signed-out">
           <SignInButton mode="modal" />
@@ -13,7 +19,10 @@ function App() {
           <UserButton />
         </Show>
       </header>
-    </>
+      <button className="btn btn-primary">Test</button>
+      <button className="btn btn-secondary">Test</button>
+      <button className="btn btn-ordinary">Test</button>
+    </Layout>
   );
 }
 
